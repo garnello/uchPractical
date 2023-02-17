d76a42ef578d53a55290b51c9ebf78721e45d406
garnello <118890237+garnello@users.noreply.github.com>
2023-02-17 17:11:56 +0700

    Add files via upload

---
+++ uchPractical5/uchPractical5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace uchPractical5
+{
+    public class Program
+    {
+        public static void Method()
+        {
+            Del del = new Del();
+        }
+        static void Main()
+        {
+            Method();
+            
+            GC.Collect();
+            Console.ReadLine();
+
+
+        }
+    }
+}

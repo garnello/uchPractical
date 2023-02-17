06e1bf7663b45bf966bf2560150d2f22b461b11c
garnello <118890237+garnello@users.noreply.github.com>
2023-02-17 14:59:15 +0700

    Add files via upload

---
+++ uchPrac1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Xml.Linq;
+
+
+namespace uPrac1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud = new Student();
+            for (int i = 0; i >= 0; i++)
+            {
+                Console.Write("Выберете пункт:\n" +
+                              "1 - Просмотр информации. \n" +
+                              "2 - Изменение данных. \n" +
+                              "3 - Выход из системы: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                switch (a)
+                {
+                    case 1:
+                        Console.Write("\n");
+                        stud.StudentInfo();
+                        Console.Write("\n");
+                        break;
+                    case 2:
+                        Console.Write("Выберете пункт: \n" +
+                            "1 - Изменить фамилию.\n" +
+                            "2 - Изменить дату рождения.\n" +
+                            "3 - Изменить номер группы студента.\n" +
+                            "4 - Изменить всю информацию о студенте: ");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        switch (b)
+                        {
+                            case 1:
+                                Console.Write("Введите фамилию: ");
+                                stud.secondName = Console.ReadLine();
+                                stud.StudentInfo();
+                                break;
+                            case 2:
+                                Console.Write("Введите дату рожденя: ");
+                                stud.dateOfBirthday = Console.ReadLine();
+                                stud.StudentInfo();
+                                break;
+                            case 3:
+                                Console.Write("Введите номер группы: ");
+                                stud.numbersGroups = Convert.ToInt32(Console.ReadLine());
+                                stud.StudentInfo();
+                                break;
+                            case 4:
+                                EditAll(stud);
+                                stud.StudentInfo();
+                                break;
+                        }
+                        break;
+                    case 3:
+                        return;
+                        break;
+                    default:
+                        Console.WriteLine("Данного варианть не имеется xD");
+                        break;
+                }
+            }
+        }
+        static void EditAll(Student stud)
+        {
+            Console.Write("Введите фамилию: ");
+            stud.secondName = Console.ReadLine();
+            Console.Write("Введите дату рождения: ");
+            stud.dateOfBirthday = Console.ReadLine();
+            Console.Write("Введите номер группы: ");
+            stud.numbersGroups = Convert.ToInt32(Console.ReadLine());
+
+            stud.progress = new int[5];
+            for (int j = 0; j < stud.progress.Length; j++)
+            {
+                Console.Write("Введите оценку:");
+                stud.progress[j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ uchPrac1/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uPrac1
+{
+    public class Student
+    {
+
+        public string secondName { get; set; }
+        public string dateOfBirthday { get; set; }
+        public int numbersGroups { get; set; }
+        public int[] progress { get; set; }
+
+        public Student()
+        {
+            secondName = "Басхалилов";
+            dateOfBirthday = "21.02.2004";
+            numbersGroups = 623;
+            progress = new int[5] { 5, 5, 5, 5, 5 };
+        }
+
+        public void StudentInfo()
+        {
+            Console.WriteLine($"Фамилия: {secondName} \n" +
+                              $"День рождения: {dateOfBirthday} \n" +
+                              $"Номер группы студента: {numbersGroups} ");
+
+            Console.Write("Успеваимость студента: ");
+            foreach (var j in progress)
+            {
+                Console.Write(j + " ");
+            }
+            Console.Write("\n");
+        }
+    }
+}
+

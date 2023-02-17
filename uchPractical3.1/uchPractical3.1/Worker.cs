d3eec294bcfa693c60be6e82664e7662bda94322
garnello <118890237+garnello@users.noreply.github.com>
2023-02-17 18:19:10 +0700

    Add files via upload

---
+++ uchPractical3.1/uchPractical3.1/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uchPractical3._1
+{
+    public class Worker
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int rate { get; set; }
+        public int days { get; set; }
+
+        public Worker() 
+        {
+            name = "Ярослав";
+            surname = "Басхалилов";
+            rate = 5;
+            days = 11;
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine($"Зарплата: {rate * days}\n");
+        }
+        public void GetInfo() 
+        {
+            Console.WriteLine($"Имя: {name}\n" +
+                              $"Фамилия: {surname}\n" +
+                              $"Ставка за день работы: {rate}\n" +
+                              $"Отработанные дни: {days}\n");
+        }
+    }
+}

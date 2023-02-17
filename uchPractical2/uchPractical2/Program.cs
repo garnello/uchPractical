34ca9fe393532d80a8134f8c1f094f945db8a5c0
garnello <118890237+garnello@users.noreply.github.com>
2023-02-17 15:18:23 +0700

    Add files via upload

---
+++ uchPractical2/uchPractical2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace uPrac2
+{
+    public class Program2
+    {
+        static void Main()
+        {
+            List<Train> tr = new List<Train>()
+            {
+                new Train(1, "Вокзал Питер", "22.21"),
+                new Train(2, "Вокзал Москва", "12.51"),
+                new Train(3, "Вокзал Екатеренбург", "02.30"),
+            };
+            Console.WriteLine("Введите номер поезда: ");
+            var numTrain = int.Parse(Console.ReadLine());
+
+            foreach (Train train in tr)
+            {
+                if (train.trainNumber == numTrain)
+                {
+                    train.TrianInfo();
+
+                }
+            }
+        }
+    }
+}
---
+++ uchPractical2/uchPractical2/Train.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uPrac2
+{
+    public class Train
+    {
+        public string destination { get; set; }
+        public int trainNumber { get; set; }
+        public string timeStart { get; set; }
+
+        public Train(int trainNumber, string destination, string timeStart)
+        {
+            this.trainNumber = trainNumber;
+            this.destination = destination;
+            this.timeStart = timeStart;
+        }
+
+        public void TrianInfo()
+        {
+            Console.Write($"Пункт отправления: {destination}\n"
+                + $"Номер поезда: {trainNumber}\n"
+                + $"Время отправления: {timeStart}");
+        }
+    }
+}
